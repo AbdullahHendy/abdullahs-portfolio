bc73b8d4502dea935b9c97548fb40eb063fc309e
Abdullah Hendy <abdullah.a.hendy@gmail.com>
2026-09-05 18:55:50 -0400

    Added github pages deployment CI workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://abdullahhendy.github.io',
+  base: '/abdullahs-portfolio',
+
   integrations: [react()],
 
   vite: {
